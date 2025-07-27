52c6c7e68e2899b88246bf5312e628c420ef3b30
Parth <parthsahani4786@gmail.com>
2025-07-27 23:23:06 +0530

    Added email and password validation check

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const UserController = require('../../controllers/user-controller');
+const AuthRequestValidatorMiddleware = require('../../middlewares/index');
 
 const router = express.Router();
 
-router.post('/user/signup', UserController.create);
+router.post('/user/signup',AuthRequestValidatorMiddleware.validateUserAuth, UserController.create);
 router.get('/user', UserController.getAll);
 router.get('/user/:id', UserController.getById);
 router.put('/user/:id', UserController.update);
